dca7f680f3282d76465dd64c64da625fd68b83e8
nishantparihar <nishprihar@gmail.com>
2024-06-24 01:19:46 +0530

    Intialized routes and prisma connection + accelerator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,57 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>().basePath('/api/v1')
 
-app.get('/', (c) => {
+
+app.post('/user/signup', (c) => {
+  
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+
   return c.text('Hello Hono!')
 })
+
+
+app.post('/user/signin', (c)=>{
+  return c.text('Hello signin')
+})
+
+
+app.post('/blog', (c) => {
+  return c.text('hello blog')
+})
+
+
+app.put('/blog', (c) => {
+  return c.text('hello blog')
+})
+
+
+app.get('/blog/bulk', (c)=>{
+  return c.text('hello bulk')
+})
+
+
+app.get('/blog/:id', (c) => {
+  return c.text(c.req.param('id'))
+})
+
+
+
+
+
+
+
+
+
 
 export default app
